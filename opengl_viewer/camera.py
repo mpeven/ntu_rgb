e293cea2b3c60b47dd306032c705a915f59ae0b5
mpeven <mpeven@gmail.com>
2017-11-07 09:29:42 -0500

    remove old op flow viewer and create directory for opengl stuff

---
+++ opengl_viewer/camera.py
@@ -0,0 +1,48 @@
+
+import numpy as np
+
+xyz0     = np.array([0., .5, -3.])
+forward0 = np.array([0., 0., 1.])
+up0      = np.array([0., 1., 0.])
+
+class Camera:
+    def __init__(self):
+        self.xyz     = xyz0.copy()
+        self.forward = forward0.copy()
+        self.up      = up0.copy()
+        self.right   = np.cross(self.forward, self.up)
+
+
+    def get_viewing_matrix(self):
+        reference_point = self.xyz + self.forward
+        all_vals = *self.xyz, *reference_point, *self.up
+        return all_vals
+
+
+    def reset(self):
+        self.xyz     = xyz0.copy()
+        self.forward = forward0.copy()
+        self.up      = up0.copy()
+        self.right   = np.cross(self.forward, self.up)
+
+
+    def rotate_camera_right(self, angle):
+        x, y, z = self.forward
+        self.forward = self.forward * np.cos(angle) - self.right * np.sin(angle)
+        self.forward /= np.linalg.norm(self.forward)
+        self.right   = np.cross(self.forward, self.up)
+
+
+    def rotate_camera_up(self, angle):
+        x, y, z = self.forward
+        self.forward = self.forward * np.cos(angle) + self.up * np.sin(angle)
+        self.forward /= np.linalg.norm(self.forward)
+        self.up = np.cross(self.forward, self.right) * -1
+
+
+    def move_forward(self, step_size):
+        self.xyz += step_size * self.forward
+
+
+    def move_right(self, step_size):
+        self.xyz += step_size * self.right
